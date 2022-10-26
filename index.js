ec4a18bdd2215e2902804bec6ba41949c81d9771
daniel-monsalve-villegas <unbigote@hush.com>
2022-10-26 18:48:58 -0400

    feat: working

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ const op = require('./services');
 const option = process.argv[2];
 const id = process.argv[3];
 
-if(option === "character" || option === "personaje") {
-	if(id !== "") {
+if(option === "characters" || option === "personajes") {
+	if(id){
 		return op.getCharacterEpisodes(id);
 	} else {
 		return op.getCharacters();
 	}
 } else if(option === "episodes" || option === "episodios") {
-	if(id !== "") {
+	if(id) {
 		return op.getEpisodeCharacters(id);
 	} else {
 		op.getEpisodes();
 	}
 }
+
